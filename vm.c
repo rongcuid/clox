ba93d80d4f54cc74977aed58db3cb012385b29ba
Rongcui Dong <rongcuid@outlook.com>
2021-11-09 11:58:55 -0500

    VM stack

--- vm.c
+++ vm.c
@@ -2,26 +2,39 @@
 
 #include <stdio.h>
 
+#include "debug.h"
+
 struct vm vm;
 
-void init_vm() {}
-
-void free_vm() {}
+static void reset_stack() {
+  vm.stack_top = vm.stack;
+}
 
 static enum interpret_result run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 
   for (;;) {
+#ifdef DEBUG_TRACE_EXECUTION
+    printf("          ");
+    for (value_t *slot = vm.stack; slot < vm.stack_top; ++slot) {
+      printf("[ ");
+      print_value(*slot);
+      printf(" ]");
+    }
+    printf("\n");
+    disassemble_instruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+#endif
     uint8_t instruction;
     switch (instruction = READ_BYTE()) {
     case OP_CONSTANT: {
       value_t constant = READ_CONSTANT();
-      print_value(constant);
-      printf("\n");
+      push(constant);
       break;
     }
     case OP_RETURN:
+      print_value(pop());
+      printf("\n");
       return INTERPRET_OK;
     }
   }
@@ -30,8 +43,24 @@ static enum interpret_result run() {
 #undef READ_CONSTANT
 }
 
+void init_vm() {
+  reset_stack();
+}
+
+void free_vm() {}
+
+
 enum interpret_result interpret(struct chunk *chunk) {
   vm.chunk = chunk;
   vm.ip = vm.chunk->code;
   return run();
+}
+
+void push(value_t value) {
+  *vm.stack_top = value;
+  ++vm.stack_top;
+}
+
+value_t pop() {
+  return *--vm.stack_top;
 }
--- vm.h
+++ vm.h
@@ -2,10 +2,15 @@
 #define __VM_H__
 
 #include "chunk.h"
+#include "value.h"
+
+#define STACK_MAX 256
 
 struct vm {
   struct chunk *chunk;
   uint8_t *ip;
+  value_t stack[STACK_MAX];
+  value_t *stack_top;
 };
 
 enum interpret_result {
@@ -18,5 +23,7 @@ void init_vm();
 void free_vm();
 
 enum interpret_result interpret(struct chunk *chunk);
+void push(value_t value);
+value_t pop();
 
 #endif // __VM_H__
