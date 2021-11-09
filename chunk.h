a8ba745d6d3619dc47e4bafc0343b526d543a7b8
Rongcui Dong <rongcuid@outlook.com>
2021-11-09 11:10:33 -0500

    Chunks done

--- chunk.h
+++ chunk.h
@@ -14,11 +14,12 @@ struct chunk {
   int count;
   int capacity;
   uint8_t *code;
+  int *lines;
   struct value_array constants;
 };
 
 void init_chunk(struct chunk *chunk);
-void write_chunk(struct chunk *chunk, uint8_t byte);
+void write_chunk(struct chunk *chunk, uint8_t byte, int line);
 void free_chunk(struct chunk *chunk);
 
 int add_constant(struct chunk *chunk, value_t value);
